f95c8d6f4923f040a270c747912836109021c20d
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-03-29 15:01:33 +0300

    Add sign in form

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, SigninForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -17,9 +17,24 @@ def auth_login():
         return render_template("auth/loginform.html", form = form,
                                error = "No such username or password")
 
-
     login_user(user)
     return redirect(url_for("index"))    
+
+@app.route("/auth/signin", methods = ["GET", "POST"])
+def auth_signin():
+    if request.method == "GET":
+        return render_template("auth/signinform.html", form = SigninForm())
+
+    print("POST - /auth/signin")
+    form = SigninForm(request.form)
+    print("form: ", form)
+    user = User(form.name.data, form.username.data, form.password.data)
+
+    db.session().add(user)
+    db.session().commit()
+
+    login_user(user)
+    return redirect(url_for("index"))
 
 @app.route("/auth/logout")
 def auth_logout():
